774ea434f9ef374c0db61faf5e014d0979244594
jo <jomirandav@hotmail.com>
2025-01-03 14:20:10 -0300

    feat(api): add GET /sets endpoint

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import pool from "./db.js";
+const port = 1337
+
+const app = express();
+app.use(express.json());
+
+// Implementa los siguientes endpoints:
+
+// GET /sets: Lista todos los sets.
+app.get('/sets', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM set');
+        res.status(200).send(result.rows);
+        // res.json(result.rows);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Server error');
+    }
+});
+
+// GET /sets/:id/cards: Lista todas las cartas de un set específico.
+
+// (Opcional) GET /cards/:id: Devuelve información detallada de una carta.
+
+
+
+//start server 
+// const PORT = process.env.PORT || 3000;   ... implementar dotenv var .env
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});

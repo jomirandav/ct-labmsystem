e96fe62499051470d3e9655d680c629ada58996b
jo <jomirandav@hotmail.com>
2025-01-03 19:56:58 -0300

    feat(api): add GET /sets/:id/cards endpoint

--- server.js
+++ server.js
@@ -20,6 +20,22 @@ app.get('/sets', async (req, res) => {
 });
 
 // GET /sets/:id/cards: Lista todas las cartas de un set específico.
+app.get('/sets/:id/cards', async (req, res) => {
+    try {
+        const set_id = [req.params.id];
+        const result = await pool.query('SELECT * FROM card INNER JOIN set ON card.set_id = set.id WHERE set.id = $1 limit 4', set_id);
+
+        // to fix: after 404, server stops
+        if (result.rows.length===0) {
+            res.status(404).send('Set id not found');
+        }
+        res.status(200).send(result.rows);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Server error');
+    }
+});
+
 
 // (Opcional) GET /cards/:id: Devuelve información detallada de una carta.
 

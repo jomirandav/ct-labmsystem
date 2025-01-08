c0d4d020a217aa2f6e74dc32706a28bab0884306
jo <jomirandav@hotmail.com>
2025-01-08 13:14:07 -0300

    style: change in sets view.

--- pk-tcg/app/sets/page.jsx
+++ pk-tcg/app/sets/page.jsx
@@ -8,26 +8,26 @@ export default function Sets() {
 
     useEffect(() => {
         fetch("http://localhost:1337/sets/")
-            .then( 
-            response => response.json()
-        )
-        .then(
-            data => {
-                console.log(data)
-                setSets([...data])
-            }
-        )
+            .then(
+                response => response.json()
+            )
+            .then(
+                data => {
+                    console.log(data)
+                    setSets([...data])
+                }
+            )
     }, [])
 
     return (
         <>
             {/* move to header component */}
             <div className='flex justify-center'>
-                <h1 className="text-sky-400 text-3xl font-bold my-6">
+                <h1 className="text-slate-700 text-3xl font-bold my-6">
                     Pokemon TCG
                 </h1>
             </div>
-            <div className='bg-sky-700 flex flex-col w-3/4 justify-self-center p-4 mb-4'>
+            <div className='rounded-md bg-sky-700 flex flex-col w-3/4 justify-self-center p-4 mb-4'>
                 <h2 className='text-xl'>Sets</h2>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere dignissimos minima quas dolorum a eaque, necessitatibus voluptate esse.</p>
             </div>
@@ -35,8 +35,8 @@ export default function Sets() {
                 <div className='flex flex-wrap justify-center'>
                     {
                         sets?.map((set, index) => (
-                            <div className='set-box text-slate-950 bg-indigo-500 hover:shadow-2xl hover:cursor-pointer size-44 m-6 p-2 rounded-md border-2 border-amber-400 content-center' key={index}>
-                                <img src={set.logo_url} className=''></img>
+                            <div className='set-box text-slate-950 bg-transparent hover:shadow-2xl hover:cursor-pointer size-44 m-6 p-2 rounded-md border-2 border-amber-400 content-center' key={index}>
+                                <img src={set.logo_url} className='hover:scale-125'></img>
                                 <ul className='hidden'>
                                     <li>
                                         {set.name}

9c7a15425767a8e8632a9bd603f8a0295279905b
jo <jomirandav@hotmail.com>
2025-01-08 13:16:17 -0300

    feat(front): add view for cards from a set

---
+++ pk-tcg/app/sets/[set_id]/page.jsx
@@ -0,0 +1,42 @@
+'use client'
+import { useParams } from 'next/navigation';
+import React, { useEffect, useState } from 'react'
+
+export default function page() {
+    const params = useParams()
+
+    const [cards, setCards] = useState([]);
+
+    useEffect(() => {
+            fetch(`http://localhost:1337/sets/${params.set_id}/cards`).then(
+                response => response.json()
+            ).then(
+                data => {
+                    setCards([...data])
+                }
+            )
+    },[])
+
+    return (
+        <>
+            <div className='flex justify-center'>
+                <h1 className="text-sky-400 text-3xl font-bold my-6">
+                    Pokemon TCG
+                </h1>
+            </div>
+            <div className='bg-sky-700 flex flex-col w-3/4 justify-self-center p-4 mb-4'>
+                <h2 className='text-xl'>Cards from *set name</h2>
+                <p>INFO DEL SET Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere dignissimos minima quas dolorum a eaque, necessitatibus voluptate esse.</p>                
+            </div>
+            <div className='flex flex-wrap justify-center'>
+                {
+                    cards?.map((card, index) => (
+                        <div className='m-6 hover:cursor-pointer hover:shadow-2xl' key={index}>
+                            <img src={card.url} className='hover:scale-105 hover:brightness-110'></img>
+                        </div>
+                    ))
+                }
+            </div>
+        </>
+    )
+}

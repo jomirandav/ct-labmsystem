8fe250c23cad56fd020ddc1f0695f1a33c9f48b4
jo <jomirandav@hotmail.com>
2025-01-09 00:42:42 -0300

    chore: clean db file

--- node_backend/db.js
+++ node_backend/db.js
@@ -1,14 +1,13 @@
 import pg from "pg";
 const { Pool } = pg;
 
-// change to read from .env?
 const pool = new Pool({
-    host: 'localhost',
+    // host: 'localhost',
+    host: 'db',
     port: 5432,
     user: 'postgres',
     password: 'mysecretpassword',
     database: 'pk_database'
 });
 
-// module.exports = pool    (commonjs)
 export default pool;

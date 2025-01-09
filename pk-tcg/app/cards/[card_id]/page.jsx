30ee29083cb32f769b5a524e53b870291b2cc622
jo <jomirandav@hotmail.com>
2025-01-09 00:35:15 -0300

    feat(front): add card details and market price links

--- pk-tcg/app/cards/[card_id]/page.jsx
+++ pk-tcg/app/cards/[card_id]/page.jsx
@@ -7,6 +7,7 @@ export default function page() {
     const params = useParams()
 
     const [card, setCard] = useState([]);
+    const market_url = 'https://prices.pokemontcg.io/';
 
     useEffect(() => {
         fetch(`http://localhost:1337/cards/${params.card_id}`).then(
@@ -21,20 +22,39 @@ export default function page() {
     return (
         <>
             <Header />
-            <div className='bg-sky-700 flex flex-col w-3/4 justify-self-center p-4 mb-14'>
-                <h2 className='text-xl'>Card</h2>
-                <p>INFO DE LA CARD Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere dignissimos minima quas dolorum a eaque, necessitatibus voluptate esse.</p>
-            </div>
-            <div className='flex justify-center mb-14'>
-                {
-                    card?.map((card, index) =>
-                        card.type == 'large' &&
-                        <div className='justify-items-center ' key={index}>
-                            <img src={card.url} className='card-detail hover:shadow-2xl hover:brightness-110'></img>
+            {
+                card?.map((card, index) =>
+                    card.image_type == 'large' &&
+                    <div key={index} className='flex justify-center my-8'>
+                            <img src={card.image_url} className='card-detail hover:shadow-2xl hover:brightness-110 w-2/5'></img>
+                        <div className='bg-sky-700 flex flex-col size-80 p-4 my-4 bg-opacity-30 rounded-md ml-8'>
+                            <h2 className='text-xl my-2'>{card.name}</h2>
+                            <ul>
+                                <li>Supertype: {card.supertype}</li>
+                                <li>Subtypes: {card.subtypes}</li>
+                                <li>Types: {card.types}</li>
+                                <li>Rarity: {card.rarity}</li>
+                            </ul>
+                            <a
+                                className="my-4 rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+                                href={`${market_url}tcgplayer/${card.id}`}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Tcgplayer price
+                            </a>
+                            <a
+                                className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+                                href={`${market_url}cardmarket/${card.id}`}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Cardmarket price
+                            </a>
                         </div>
-                    )
-                }
-            </div>
+                    </div>
+                )
+            }
         </>
     )
 }
